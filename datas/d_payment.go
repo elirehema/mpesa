005d0e29066e1dcf632804168e5485ccd50b0c27
Elirehema Paul <rocksandystone@gmail.com>
2023-06-26 15:27:20 +0300

    Query beneficiary name

--- datas/d_payment.go
+++ datas/d_payment.go
@@ -1,6 +1,6 @@
 package datas
 
-type PaymentData struct {
+type RequestData struct {
 	Amount                 string `json:"input_Amount,omitempty"`
 	Country                string `json:"input_Country,omitempty"`
 	Currency               string `json:"input_Currency,omitempty"`
@@ -13,4 +13,10 @@ type PaymentData struct {
 	ServiceProviderCode    string `json:"input_ServiceProviderCode,omitempty"`
 	PaymentItemDescription string `json:"input_PaymentItemsDesc,omitempty"`
 	QueryReference         string `json:"input_QueryReference,omitempty"`
+	OriginalConversationId string `json:"input_OriginalConversationID,omitempty"`
+	CustomerFirstName      string `json:"input_CustomerFirstName,omitempty"`
+	CustomerLastName       string `json:"input_CustomerLastName,omitempty"`
+	ResultCode             string `json:"input_ResultCode,omitempty"`
+	ResultDescription      string `json:"input_ResultDesc,omitempty"`
+	KYCQueryType           string `json:"input_KYCQueryType,omitempty"`
 }
